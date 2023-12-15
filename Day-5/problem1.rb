5fee9c207447f4d25456391a273ba09f6b33f6f7
Simone Roveri <spikeroveri91@gmail.com>
2023-12-15 19:50:32 +0900

    working on the logic

--- Day-5/problem1.rb
+++ Day-5/problem1.rb
@@ -1,17 +1,29 @@
 data = File.read("file.txt").split(/[-a-zA-Z:]+/).reject(&:empty?)
 
-p "seeds -> #{seeds = data.first}"
-p "    "
-p "seed-to-soil -> #{to_soils = data[2]}"
-p "    "
-p "soil-to-fertilizer -> #{to_fertilizer = data[4]}"
-p "    "
-p "fertilizer-to-water -> #{to_water = data[6]}"
-p "    "
-p "water-to-light -> #{to_light = data[8]}"
-p "    "
-p "light-to-temperature -> #{to_temperature = data[10]}"
-p "    "
-p "temperature-to-humidity -> #{to_humidity = data[12]}"
-p "    "
-p "humidity-to-location -> #{to_location = data[14]}"
+seeds = data.first
+seed_to_soils = data[2]
+soil_to_fertilizer = data[4]
+fertilizer_to_water = data[6]
+water_to_light = data[8]
+light_to_temperature = data[10]
+temperature_to_humidity = data[12]
+humidity_to_location = data[14]
+
+
+def craete_hash(data)
+  hash = {}
+  data.split("\n").each do |line|
+    # p "line -> #{line}" unless line.empty?
+    unless line.empty?
+      # p line.split(" ")[0].to_i
+      (0..line.split(" ").last.to_i - 1).each do |index|
+        hash[line.split(" ")[0].to_i + index ] = line.split(" ")[1].to_i + index
+        # p "hash[#{line.split(" ")[0].to_i + index}] =====>>  #{hash[line.split(" ")[0].to_i + index ]}"
+        p hash
+      end
+    end
+
+  end
+end
+
+craete_hash(seed_to_soils)
